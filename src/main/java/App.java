26e06c122a61dd258a19ddf7703f9a9ab2413065
Kiarahmani <rahmank@purdue.edu>
2019-07-06 19:05:42 -0400

    edit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,22 +1,38 @@
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
-
-import com.mysql.jdbc.Driver;
-import com.mysql.jdbc.exceptions.jdbc4.MySQLTransactionRollbackException;
+import java.util.Properties;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String opType = args[0];
-		int insID = Integer.valueOf(args[1]);
-		Transaction txn = new Transaction(insID);
-		txn.run(opType);
+		try {
+			Connection connect = null;
+			PreparedStatement preparedStatement = null;
+			ResultSet rs = null;
+			Properties p;
+			p = new Properties();
+			p.setProperty("ID", "1");
+			p.setProperty("dbName", "feedback");
+
+			Driver driver = DriverManager.getDriver("jdbc:cassandra://localhost:9042/testks");
+			System.out.println("driver: " + driver);
+			connect = driver.connect("", p);
+			System.out.println("connect: " + connect);
+
+			preparedStatement = connect.prepareStatement("select * from feedback.kv where id=?");
+			preparedStatement.setInt(1, 1);
+			// Thread.sleep(2500);
+			rs = preparedStatement.executeQuery();
+			rs.next();
+			System.out.println("(" + rs.getInt("id") + "," + rs.getInt("value") + ")");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// close();
+		}
 
 	}
 
